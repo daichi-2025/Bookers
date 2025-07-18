c4fe022321fc0844f94011b2bc57e988f297166e
EC2 Default User <ec2-user@ip-172-31-38-20.ap-northeast-1.compute.internal>
2025-07-18 03:12:27 +0000

    [Add]form_with_model

---
+++ app/controllers/bookers_controller.rb
@@ -0,0 +1,14 @@
+class BookersController < ApplicationController
+  def new
+    @booker = Booker.new
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  root to: 'homes#top'
+  resources :posts
+  get 'bookers/new'
+  post 'bookers' => 'bookers#create'
+  get 'bookers/index'
+  get 'bookers/show'
+  get 'bookers/edit'
+  get '/top' => "homes#top"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
