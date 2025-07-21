2add0430c77bb98659a240d5987d9df30b54884d
EC2 Default User <ec2-user@ip-172-31-38-20.ap-northeast-1.compute.internal>
2025-07-21 04:05:07 +0000

    [Add]bookのshowページの作成

--- app/controllers/bookers_controller.rb
+++ app/controllers/bookers_controller.rb
@@ -1,19 +1,20 @@
 class BookersController < ApplicationController
   def new
-    @bookers = Booker.all
     @booker = Booker.new
   end
 
   def create
     booker = Booker.new(booker_params)
     booker.save
-    redirect_to '/top'
+    redirect_to booker_path(booker.id)
   end
 
   def index
+    @bookers = Booker.all
   end
 
   def show
+    @booker = Booker.find(params[:id])
   end
 
   def edit
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   resources :posts
   get 'bookers/new'
   post 'bookers' => 'bookers#create'
-  get 'bookers/new'
-  get 'bookers/show'
+  get 'bookers/index'
+  get 'bookers/:id' => "bookers#show", as: 'booker'
   get 'bookers/edit'
   get '/top' => "homes#top"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
