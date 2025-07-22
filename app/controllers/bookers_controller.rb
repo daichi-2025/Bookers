810b0b28a260106a2d797d7608cabfe8422a77f7
EC2 Default User <ec2-user@ip-172-31-38-20.ap-northeast-1.compute.internal>
2025-07-22 14:26:58 +0000

    [Add]update機能の作成

--- app/controllers/bookers_controller.rb
+++ app/controllers/bookers_controller.rb
@@ -18,6 +18,19 @@ class BookersController < ApplicationController
   end
 
   def edit
+    @booker = Booker.find(params[:id])
+  end
+
+  def update
+    booker = Booker.find(params[:id])
+    booker.update(booker_params)
+    redirect_to booker_path(booker.id)
+  end
+
+  def destroy
+    booker = Booker.find(params[:id])
+    booker.destroy
+    redirect_to '/bookers'
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
   post 'bookers' => 'bookers#create'
   get 'bookers/index'
   get 'bookers/:id' => "bookers#show", as: 'booker'
-  get 'bookers/edit'
+  get 'bookers/:id/edit' => 'bookers#edit', as: 'edit_booker'
   get '/top' => "homes#top"
+  patch 'bookers/:id' => 'bookers#update', as: 'update_booker'
+  delete 'bookers/:id' => 'bookers#destroy', as: 'destroy_booker'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
